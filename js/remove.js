24cae773814f59318af195d393b8a20f30059e87
Moxus <moxus@outlook.com>
2016-09-03 17:58:46 +0200

    Sanfteres Vorgehen gegen Cookies

--- js/remove.js
+++ js/remove.js
@@ -1,8 +1,25 @@
+var EXPIRATION = (60*60*8);
 chrome.webNavigation.onCommitted.addListener(function(e) {
-	chrome.cookies.remove({
-		url: 'http://derstandard.at/',
-		name: 'MGUID'
-	});
-	},
-	{url: [{hostSuffix: 'derstandard.at'}]}
+	if (e.url.includes('derstandard.at')) {
+		chrome.cookies.get({
+			url: 'http://derstandard.at/',
+			name: 'MGUID'
+		}, function(cookie){
+			var d = new Date();
+			var t = d.valueOf()/1000;
+			if(cookie.expirationDate > t+EXPIRATION) {
+				chrome.cookies.set({
+					url: 'http://derstandard.at/',
+					name: cookie.name,
+					value: cookie.value,
+					domain: cookie.domain,
+					path: cookie.path,
+					secure: cookie.secure,
+					httpOnly: cookie.httpOnly,
+					expirationDate: t+EXPIRATION,
+					storeId: cookie.storeId
+				});
+			}
+		});
+    }}
 );
